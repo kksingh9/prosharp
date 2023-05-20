304439028d28d50f43b288315266e61d8b11c179
kksingh9 <kksingh0921@gmail.com>
2023-05-21 00:31:53 +0530

    react component

---
+++ expenseitem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return (<div><h1>ExpenseItem!</h1>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+            </div>);
+
+}
+
+export default ExpenseItem
