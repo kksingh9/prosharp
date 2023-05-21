6327197fd825090b57eb1dd3bc41afa6804c9f3b
kksingh9 <kksingh0921@gmail.com>
2023-05-21 18:24:49 +0530

    props

--- expenseitem.js
+++ expenseitem.js
@@ -1,8 +1,11 @@
-function ExpenseItem(){
-    return (<div><h1>ExpenseItem!</h1>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+import './expenseitem.css'
+function ExpenseItem(props){
+   
+    return (<div className='expense-item'><h1>{props.data.toISOString()}</h1>
+            <div className='expense-item__description'><h2>{props.explocation}</h2></div>
+            <p className='expense-item__price'>  {props.title}</p>
+            <p className='expense-item__price'>Rs {props.amount}</p>
+            
             </div>);
 
 }
