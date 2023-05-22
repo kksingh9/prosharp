a98a512637f37f4ec0450074c7641c3572477170
kksingh9 <kksingh0921@gmail.com>
2023-05-23 00:32:47 +0530

    add deletebtn

--- Expenses/expenseitem.js
+++ Expenses/expenseitem.js
@@ -3,15 +3,20 @@ import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 import Card from '../UI/Card';
-import './expenseitem.css'
+import './expenseitem.css';
+
 
 const ExpenseItem =(props) => {
-    
+    const clickHandler = () => {
+        console.log('Clicked!!!');
+    }
     return (
             <Card className="expense-item">
             <ExpenseDate date={props.date} />
             
             <ExpenseDetail title = {props.title} explocation={props.explocation} amount={props.amount} />
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={clickHandler}>Delete Title</button>
             </Card>
             );
 
