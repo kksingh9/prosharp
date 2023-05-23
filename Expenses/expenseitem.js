61104e4b236fa058d16acd0d25ce263c24a8656d
kksingh9 <kksingh0921@gmail.com>
2023-05-23 17:10:08 +0530

    button for expense

--- Expenses/expenseitem.js
+++ Expenses/expenseitem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
@@ -7,16 +7,28 @@ import './expenseitem.css';
 
 
 const ExpenseItem =(props) => {
+    //useState is a react hook
+    
+    const [title, setTitle] = useState(props.title)
+    const [amount, setTitle1] = useState(props.amount)
+    //let title = props.title;
+    
     const clickHandler = () => {
-        console.log('Clicked!!!');
+        
+        setTitle('update')
+        console.log(title);
+    }
+    const expenamount = () => {
+        setTitle1('100')
     }
     return (
             <Card className="expense-item">
             <ExpenseDate date={props.date} />
             
-            <ExpenseDetail title = {props.title} explocation={props.explocation} amount={props.amount} />
+            <ExpenseDetail title = {title} explocation={props.explocation} amount={amount} />
             <button onClick={clickHandler}>Change Title</button>
-            <button onClick={clickHandler}>Delete Title</button>
+            <button onClick={expenamount}>Change expense</button>
+        
             </Card>
             );
 
